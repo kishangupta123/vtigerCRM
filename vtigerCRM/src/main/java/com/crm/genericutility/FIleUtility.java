71597a760b325aafa014c77032bf190d8250117d
kishangupta123 <96646948+kishangupta123@users.noreply.github.com>
2024-11-25 00:05:27 +0530

    added fileutility and some part of webdriver utility file

--- vtigerCRM/src/main/java/com/crm/genericutility/FIleUtility.java
+++ vtigerCRM/src/main/java/com/crm/genericutility/FIleUtility.java
@@ -1,5 +1,25 @@
 package com.crm.genericutility;
 
-public class FIleUtility {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+/**
+ * @author Kishan Kumar Gupta
+ */
+public class FIleUtility {
+	/**
+	 * This method is used to read data from property file.
+	 * This method taskes key as a parameter and returns value.
+	 * @param key
+	 * @return
+	 * @throws IOException
+	 */
+	
+	public String getDataFromProperty(String key) throws IOException {
+		FileInputStream fis = new FileInputStream("./src/test/resources/TestData/commonData.properties");
+		Properties prop = new Properties();
+		prop.load(fis);
+		return prop.getProperty(key);
+	}
 }
--- vtigerCRM/src/main/java/com/crm/genericutility/WebDriverUtility.java
+++ vtigerCRM/src/main/java/com/crm/genericutility/WebDriverUtility.java
@@ -1,5 +1,60 @@
 package com.crm.genericutility;
 
-public class WebDriverUtility {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * @author Kishan Kumar Gupta
+ */
+
+public class WebDriverUtility {
+	/**
+	 * Made Webdriver and Actions class public so that it can be used throughout the
+	 * project
+	 */
+	public WebDriver driver;
+	public Actions action;
+
+	/**
+	 * This method is used to maximize the window
+	 */
+	public void maximizeWindow() {
+		driver.manage().window().maximize();
+	}
+
+	/**
+	 * This method is used to minimize the window
+	 */
+	public void minimizeWindow() {
+		driver.manage().window().minimize();
+	}
+
+	/**
+	 * This method is used to delete all the cookies.
+	 */
+	public void deleteAllCookies() {
+		driver.manage().deleteAllCookies();
+	}
+
+	/**
+	 * This method is used to add implicit wait for the webpage
+	 */
+	public void implicitWait() {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); 
+	}
+	
+	/**
+	 * This method is used to add explicit wait for the webpage
+	 */
+	public void explicitWait() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	}
+	
+	public void launchBrowser() {
+		driver.get(null);
+	}
+	
 }
