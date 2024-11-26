26508cdf219c1fc3df4bd3519036899fa6bde306
kishangupta123 <96646948+kishangupta123@users.noreply.github.com>
2024-11-27 04:14:13 +0530

    BaseClass

--- vtigerCRM/src/main/java/com/crm/genericutility/BaseClass.java
+++ vtigerCRM/src/main/java/com/crm/genericutility/BaseClass.java
@@ -81,7 +81,7 @@ public class BaseClass {
 			Reporter.log("Invlaid browser required");
 		}
 
-		driver.manage().window().maximize();
+		wutils.maximizeWindow();
 		sdriver = driver;
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
